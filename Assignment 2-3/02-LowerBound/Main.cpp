32dd853e2b1537b90d52a0fe7387b95251849e5f
Chen Zhou <chenzhoux93@gmail.com>
2020-09-29 23:22:26 -0400

    Assignment 2-3

---
+++ Assignment 2-3/02-LowerBound/Main.cpp
@@ -0,0 +1,67 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// ================= 代码实现开始 =================
+
+/* 请在这里定义你需要的全局变量 */
+
+
+// 本函数传入数组 a 及所有询问，你需要求解所有询问并一并返回
+// n：序列 a 的长度
+// a：存储了序列 a
+// Q：询问个数
+// query：依次存储了所有询问的参数 x
+// 返回值：一个 vector<int>，依次存放各询问的答案
+vector<int> getAnswer(int n, vector<int> a, int Q, vector<int> query) {
+    /* 请在这里设计你的算法 */
+    vector<int> ans;
+    ans.clear();
+    sort(a.begin(), a.end());
+
+    for(int i=0; i<Q; ++i){
+        int key = query[i];
+        int l=0, r=n, mid;
+        while(l < r){
+            mid = (l+r) >> 1;
+            cout << "mid: " << mid << endl;
+            if(a[mid] < key){
+                l = mid+1;
+            }else{
+                r = mid;
+            }
+            cout << l << " " << r << endl;
+        }
+        int pos = mid;
+        if(pos >= n){
+            ans.push_back(-1);
+        }else{
+            ans.push_back(a[pos]);
+        }
+    }
+    return ans;
+}
+
+// ================= 代码实现结束 =================
+
+int main() {
+    int n, Q, tmp;
+    vector<int> a, query;
+    a.clear();
+    query.clear();
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &tmp);
+        a.push_back(tmp);
+    }
+    scanf("%d", &Q);
+    for (int i = 0; i < Q; ++i) {
+        scanf("%d", &tmp);
+        query.push_back(tmp);
+    }
+    vector<int> ans = getAnswer(n, a, Q, query);
+    for (int i = 0; i < Q; ++i)
+        printf("%d\n", ans[i]);
+    return 0;
+}
