6993bb1f025812c9d037b250c79a92cdee5fa06f
Chen Zhou <chenzhoux93@gmail.com>
2020-10-13 23:16:16 -0400

    Assignment 4-1

--- Assignment 4-1/02-palindromic/Main.cpp
+++ Assignment 4-1/02-palindromic/Main.cpp
@@ -1,35 +1,46 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 // ================= 代码实现开始 =================
 
-const int N = 1005;
+const int N = 500005;
+char s[N * 2];
+int len[N * 2];
 
 /* 请在这里定义你需要的全局变量 */
 
-// 为了减少复制开销，我们直接读入信息到全局变量中
-// a, b：题目所述数组，a的大小为(n+1)*(m+1)，b的大小为(p+1)*(q+1)，下标均从1开始有意义（下标0无意义，你可以直接无视）
-// n, m, p, q：题中所述
-int a[N][N], b[N][N], n, m, p, q;
-
-// 求出a中有哪些位置出现了b
-// 返回值：一个pair<int, int>的数组，包含所有出现的位置
-vector<pair<int, int>> getAnswer() {
+// 计算str中有多少个回文子串
+// 返回值：子串的数目
+long long getAnswer(string str) {
     /* 请在这里设计你的算法 */
-} 
+    int n = str.size();
+    for(int i = n; i; --i){
+        s[i << 1] = str[i-1], s[i << 1 | 1] = 0;
+    }
+        
+    n = n << 1 | 1;
+    s[1] = 0,s[0] = 1,s[n+1] = 2;
+    int cur = 1;
+    long long ans =0;
+    for(int i=2; i<=n; ++i){
+        int &now = len[i], pos = (cur << 1) -i;
+        now = max(min(len[pos],cur+len[cur]-i),0);
+        while(s[i-now-1] == s[i + now +1])
+            ++now;
+        if(i + now > cur +len[cur])
+            cur = i;
+        ans += (now + 1) >> 1;
+    }
+    return ans;
+}
 
 // ================= 代码实现结束 =================
 
+char _s[N];
+
 int main() {
-    scanf("%d%d%d%d", &n, &m, &p, &q);
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j <= m; ++j)
-            scanf("%d", &a[i][j]);
-    for (int i = 1; i <= p; ++i)
-        for (int j = 1; j <= q; ++j)
-            scanf("%d", &b[i][j]);
-    vector<pair<int, int>> ans = getAnswer();
-    for (int i = 0; i < int(ans.size()); ++i)
-        printf("%d %d\n", ans[i].first, ans[i].second);
+    scanf("%s", _s + 1);
+    printf("%lld\n", getAnswer(_s + 1));
     return 0;
 }
